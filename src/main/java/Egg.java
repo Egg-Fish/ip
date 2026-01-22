c219ea0b42278581af9f7d9040e1e6e2adc6c847
Egg-Fish <eugeniomanansala@gmail.com>
2026-01-22 10:39:58 +0800

    level 0

---
+++ src/main/java/Egg.java
@@ -0,0 +1,12 @@
+public class Egg {
+    public static void main(String[] args) {
+        String hl = "------------------------------------------";
+
+        System.out.println(hl);
+        System.out.println("Hello! I'm Egg");
+        System.out.println("What can I do for you?");
+        System.out.println(hl);
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
