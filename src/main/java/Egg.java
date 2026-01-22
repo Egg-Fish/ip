06182ec067a19cfa6cf9c3a5ec00886d4d9b2af0
Egg-Fish <eugeniomanansala@gmail.com>
2026-01-22 10:54:09 +0800

    level 1

--- src/main/java/Egg.java
+++ src/main/java/Egg.java
@@ -1,12 +1,43 @@
+import java.util.Scanner;
+
 public class Egg {
-    public static void main(String[] args) {
+    private static void print(String s, int indent){
+        System.out.println(" ".repeat(indent) + s);
+    }
+
+     private static void print(String s){
+         print(s, 4);
+    }
+
+    private static void printMessage(String message) {
         String hl = "------------------------------------------";
+        
+        print(hl);
+        
+        for (String m : message.split("\n")) {
+            print(m);
+        }
+        
+        print(hl);
+    }
+    
+    public static void main(String[] args) {
+        printMessage("Hello! I'm Egg \nWhat can I do for you?");
 
-        System.out.println(hl);
-        System.out.println("Hello! I'm Egg");
-        System.out.println("What can I do for you?");
-        System.out.println(hl);
+        
 
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String msg = scanner.nextLine();
+
+            if (msg.equals("bye")) {
+                break;
+            }
+
+            printMessage(msg);
+        }
+
+        printMessage("Bye. Hope to see you again soon!");
     }
 }
