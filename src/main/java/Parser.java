a7c13263c9c7e44844ddf06a551a2f85baff0315
Egg-Fish <eugeniomanansala@gmail.com>
2026-01-29 18:20:49 +0800

    Parser: Add deadline task parsing

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,6 +1,8 @@
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+import java.time.LocalDate;
+
 public class Parser {
     public static Command parseTodoCommand(String commandString) {
         Pattern pattern = Pattern.compile("^todo\\s+([^\\n]+?)\\s*$");
@@ -17,6 +19,29 @@ public class Parser {
             throw new RuntimeException("Could not parse as todo command: " + commandString);
         }
     }
+
+    public static Command parseDeadlineCommand(String commandString) {
+        Pattern pattern = Pattern.compile("^deadline\\s+([^\\n]+?)\\s+/by\\s+([^\\n]+?)\\s*$");
+        Matcher matcher = pattern.matcher(commandString);
+        
+        if (matcher.matches()) {
+            String description = matcher.group(1);
+            String byString = matcher.group(2);
+
+            try {
+                LocalDate by = LocalDate.parse(byString);
+                    
+                Task newTask = new DeadlineTask(description, by);
+                Command command = new AddCommand(newTask);
+            
+                return command;
+            } catch (Exception e) {
+                throw new RuntimeException("Could not parse as date: " + byString);
+            }
+        } else {
+            throw new RuntimeException("Could not parse as deadline command: " + commandString);
+        }
+    }
     
     public static Command parseCommand(String commandString) {
         String first = commandString.trim().split(" ", 2)[0];
@@ -24,6 +49,8 @@ public class Parser {
         switch (first) {
         case "todo":
             return parseTodoCommand(commandString);
+        case "deadline":
+            return parseDeadlineCommand(commandString);
         case "bye":
             return new ByeCommand();
         default:
