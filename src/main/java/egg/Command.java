b2dd6877c2ca9435e0afb2e7203a4c1bd18ba09b
Egg-Fish <eugeniomanansala@gmail.com>
2026-02-04 14:01:25 +0800

    Add JavaDoc to Command

--- src/main/java/egg/Command.java
+++ src/main/java/egg/Command.java
@@ -1,5 +1,17 @@
 package egg;
 
+/**
+ * Represents an executable command in the application.
+ * This is an abstract base class for all specific command implementations, 
+ * such as adding, deleting, or marking tasks.
+ */
 public abstract class Command {
+    
+    /**
+     * Executes the specific command.
+     * @param tasks   The {@link TaskList} instance.
+     * @param ui      The {@link Ui} instance.
+     * @param storage The {@link Storage} instance.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage);
 }
