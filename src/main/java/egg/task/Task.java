c2ca8d8fe27ed145580c79758d3d42aea0d0f38f
Egg-Fish <eugeniomanansala@gmail.com>
2026-02-18 13:54:35 +0800

    Add tags to Task class and tag parsers to subclasses

--- src/main/java/egg/task/Task.java
+++ src/main/java/egg/task/Task.java
@@ -1,25 +1,39 @@
 package egg.task;
 
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Represents a task in the system.
  * A task consists of a description and a completion status.
  */
 public class Task {
+    private static Pattern tagPattern =
+        Pattern.compile("#([^ ]+)");
+
     /** The textual description of the task. */
     protected String description;
 
     /** The status of the task, where true indicates completion. */
     protected boolean isDone;
 
+    /** The tags associated with the task. */
+    protected HashSet<String> tags;
+
     /**
      * Initializes a new Task with the specified description.
-     * By default, the task is marked as not done.
+     * By default, the task is marked as not done, and has
+     * no tags.
      *
      * @param description The description of the task.
      */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        this.tags = new HashSet<String>();
     }
 
     public String getDescription() {
@@ -38,6 +52,39 @@ public class Task {
      */
     public void unmark() {
         isDone = false;
+    }
+
+    /**
+     * Adds a tag to the task.
+     */
+    public void tag(String tag) {
+        this.tags.add(tag);
+    }
+
+    /**
+     * Removes a tag from the task.
+     */
+    public void untag(String tag) {
+        this.tags.remove(tag);
+    }
+
+    public String getTagString() {
+        String tagString = "";
+        for (String tag : tags) {
+            tagString += "#" + tag + " ";
+        }
+
+        return tagString.trim();
+    }
+
+    public void addTagsFromString(String tagString) {
+        Matcher matcher = tagPattern.matcher(tagString);
+
+        while (matcher.find()) {
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                this.tags.add(matcher.group(i));
+            }
+        }
     }
 
     @Override
